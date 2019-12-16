fe1a2ae116f5f24e0df35cbcd855039c57a86a38
wktg623 <wktg623@gmail.com>
2019-12-15 18:40:18 -0600

    could not complete search form--but I think am very close

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,86 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const WholePage = styled.div
+`
+display: flex;
+flex-direction: row-reverse;
+flex-wrap: nowrap;
+justify-content: center;
+
+
+`
+
+const Name = styled.div`
+
+
+color: blue;
+font-size: 32px;
+
+
+`
+
+const Image = styled.div
+`
+text-align: center;
+
+border-radius: 100%;
+
+
+`
+const Status = styled.div
+
+`
+color: blue;
+font-size: 18px;
+
+
+`
+
+const Species = styled.div
+
+`
+color: blue;
+font-size: 18px;
+
+`
+
+const Info = styled.div
+
+`
+display: flex;
+width: 30%;
+margin: 0 auto;
+flex-direction: column;
+
+text-align: center;
+box-sizing: content-box;
+border: solid black 1px;
+background: coral;
+margin-bottom: 10px;
+margin-top: 10px;
+`
+
+export default function CharacterCard(props) {
+
+  console.log("props", props.character)
+  return(
+    <div>
+    <WholePage>
+  <Info>
+  <Name>
+      <h2> {props.character.name}</h2>
+  </Name>
+  
+  <Status>
+      <p>Status: {props.character.status}</p>
+      </Status>
+      <Species>
+      <p>Species: {props.character.species}</p>
+</Species>
+    </Info>
+    <Image ><img src = {props.character.image}/></Image>
+    </WholePage>
+    </div>
+  )
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,44 @@
 import React from "react";
+import {Route} from "react-router-dom";
+import CharacterList from "./CharacterList";
+import CharacterCard from "./CharacterCard";
+import App from "../App";
+import styled from "styled-components";
+
+
+
+
+const Main = styled.div`
+
+color: blue;
+
+
+
+`
+const headerImg = styled.div`
+
+border-radius: 100%;
+`
+
 
 export default function WelcomePage() {
   return (
+    <Main>
     <section className="welcome-page">
       <header>
+        <mainTitle>
         <h1>Welcome to the ultimate fan site!</h1>
+        </mainTitle>
+       <headerImg>
         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
+        </headerImg>
       </header>
     </section>
+    </Main>
   );
 }
+
